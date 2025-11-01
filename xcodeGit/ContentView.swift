5cc573feae9e53d33d4acb5a9c7d06bcb6b54480
creciente1974-afk <creciente1974@gmail.com>
2025-11-02 08:55:02 +0900

    二回目

--- xcodeGit/ContentView.swift
+++ xcodeGit/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Text("はじめてのコミット")
+            Text("二回目のコミット")
         }
         .padding()
     }
