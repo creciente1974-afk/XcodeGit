60aabfcc29b78f6d2f64c4239504d510dbea6bab
creciente1974-afk <creciente1974@gmail.com>
2025-11-01 18:17:11 +0900

    Initial Commit

---
+++ xcodeGit/xcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeGitApp.swift
+//  xcodeGit
+//
+//  Created by tsuda kazumi on 2025/11/01.
+//
+
+import SwiftUI
+
+@main
+struct xcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
